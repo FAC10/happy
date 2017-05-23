1017f29374c39d43b9b76a0fd180d7e6da56667e
Joey Scott <joannascott29@hotmail.co.uk>
2017-05-23 15:19:45 +0100

    Add event listeners to sign up and login buttons Relates #32

--- public/index.js
+++ public/index.js
@@ -583,3 +583,27 @@ d3.select('#reset').on('click', function() {
 // to run.
 
 initForce();
+
+
+var tab = document.querySelector('.tabs').childNodes;
+var signUpLink = tab[1].firstChild;
+var loginLink = tab[3].firstChild;
+
+var signUpContent = document.getElementById('signup-tab-content');
+var loginContent = document.getElementById('login-tab-content');
+
+signUpLink.addEventListener('click', function(e){
+  e.preventDefault();
+  signUpLink.classList.add('active');
+  signUpContent.classList.add('active');
+  loginLink.classList.remove('active');
+  loginContent.classList.remove('active');
+});
+
+loginLink.addEventListener('click', function(e){
+  e.preventDefault();
+  loginLink.classList.add('active');
+  loginContent.classList.add('active');
+  signUpLink.classList.remove('active');
+  signUpContent.classList.remove('active');
+});
