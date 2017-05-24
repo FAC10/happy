d8dec0ace6aebe6a0fe5652157aa3684333d6dea
pbywater <philippabywater@gmail.com>
2017-05-24 15:11:49 +0100

    correcting animation on safari
    relates to #54

--- public/index.js
+++ public/index.js
@@ -598,7 +598,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 $('#pencil').on('click', () => {
   console.log('clicked');
-  $('.menu-open:checked ~ #pencil').addClass('wipe');
+  $('.menu-open:checked ~ #pencil').removeClass('normal').addClass('wipe');
   setTimeout(function () {
     $('.menu-open:checked ~ #pencil').addClass('finished');
       $('.menu-open-button, .hamburger').fadeIn().addClass('active');
@@ -606,7 +606,7 @@ $('#pencil').on('click', () => {
   $('.menu-open:checked ~ #camera, #microphone, #cog, #clock').fadeOut();
   $('.menu-open-button').fadeOut();
   $('.menu-open-button').on('click', () => {
-    $('.menu-open:checked ~ #pencil').removeClass('wipe');
+    $('.menu-open:checked ~ #pencil').removeClass('wipe').addClass('normal');
     $('.menu-open:checked ~ #pencil').removeClass('finished');
       $('.menu-open-button, .hamburger').removeClass('active');
         $('.menu-open:checked ~ #camera, #microphone, #cog, #clock').fadeIn();
@@ -614,7 +614,7 @@ $('#pencil').on('click', () => {
 });
 $('#camera').on('click', () => {
   console.log('clicked');
-  $('.menu-open:checked ~ #camera').addClass('wipe');
+  $('.menu-open:checked ~ #camera').removeClass('normal').addClass('wipe');
   setTimeout(function () {
     $('.menu-open:checked ~ #camera').addClass('finished');
       $('.menu-open-button, .hamburger').fadeIn().addClass('active');
@@ -622,7 +622,7 @@ $('#camera').on('click', () => {
   $('.menu-open:checked ~ #pencil, #microphone, #cog, #clock').fadeOut();
   $('.menu-open-button').fadeOut();
   $('.menu-open-button').on('click', () => {
-    $('.menu-open:checked ~ #camera').removeClass('wipe');
+    $('.menu-open:checked ~ #camera').removeClass('wipe').addClass('normal');
     $('.menu-open:checked ~ #camera').removeClass('finished');
       $('.menu-open-button, .hamburger').removeClass('active');
         $('.menu-open:checked ~ #pencil, #microphone, #cog, #clock').fadeIn();
@@ -632,13 +632,13 @@ $('#microphone').on('click', () => {
   console.log('clicked');
   $('.menu-open:checked ~ #microphone').addClass('wipe');
   setTimeout(function () {
-    $('.menu-open:checked ~ #microphone').addClass('finished');
+    $('.menu-open:checked ~ #microphone').removeClass('normal').addClass('finished');
       $('.menu-open-button, .hamburger').fadeIn().addClass('active');
   }, 1000);
   $('.menu-open:checked ~ #pencil, #camera, #cog, #clock').fadeOut();
   $('.menu-open-button').fadeOut();
   $('.menu-open-button').on('click', () => {
-    $('.menu-open:checked ~ #microphone').removeClass('wipe');
+    $('.menu-open:checked ~ #microphone').removeClass('wipe').addClass('normal');
     $('.menu-open:checked ~ #microphone').removeClass('finished');
       $('.menu-open-button, .hamburger').removeClass('active');
         $('.menu-open:checked ~ #pencil, #camera, #cog, #clock').fadeIn();
@@ -646,7 +646,7 @@ $('#microphone').on('click', () => {
 });
 $('#clock').on('click', () => {
   console.log('clicked');
-  $('.menu-open:checked ~ #clock').addClass('wipe');
+  $('.menu-open:checked ~ #clock').removeClass('normal').addClass('wipe');
   setTimeout(function () {
     $('.menu-open:checked ~ #clock').addClass('finished');
       $('.menu-open-button, .hamburger').fadeIn().addClass('active');
@@ -654,7 +654,7 @@ $('#clock').on('click', () => {
   $('.menu-open:checked ~ #pencil, #camera, #cog, #microphone').fadeOut();
   $('.menu-open-button').fadeOut();
   $('.menu-open-button').on('click', () => {
-    $('.menu-open:checked ~ #clock').removeClass('wipe');
+    $('.menu-open:checked ~ #clock').removeClass('wipe').addClass('normal');
     $('.menu-open:checked ~ #clock').removeClass('finished');
       $('.menu-open-button, .hamburger').removeClass('active');
         $('.menu-open:checked ~ #pencil, #camera, #cog, #microphone').fadeIn();
@@ -662,7 +662,7 @@ $('#clock').on('click', () => {
 });
 $('#cog').on('click', () => {
   console.log('clicked');
-  $('.menu-open:checked ~ #cog').addClass('wipe');
+  $('.menu-open:checked ~ #cog').removeClass('normal').addClass('wipe');
   setTimeout(function () {
     $('.menu-open:checked ~ #cog').addClass('finished');
       $('.menu-open-button, .hamburger').fadeIn().addClass('active');
@@ -670,7 +670,7 @@ $('#cog').on('click', () => {
   $('.menu-open:checked ~ #pencil, #camera, #clock, #microphone').fadeOut();
   $('.menu-open-button').fadeOut();
   $('.menu-open-button').on('click', () => {
-    $('.menu-open:checked ~ #cog').removeClass('wipe');
+    $('.menu-open:checked ~ #cog').removeClass('wipe').addClass('normal');
     $('.menu-open:checked ~ #cog').removeClass('finished');
       $('.menu-open-button, .hamburger').removeClass('active');
         $('.menu-open:checked ~ #pencil, #camera, #clock, #microphone').fadeIn();
