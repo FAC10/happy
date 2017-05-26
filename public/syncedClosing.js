3e0779671eb196ee9417a80d14b1c1976caf76b0
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-26 16:04:07 +0100

    now the menu wont open whenthe modal is open relates #96

--- public/syncedClosing.js
+++ public/syncedClosing.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-  if($('.modal').hasClass('show') && $('.menu-open:checked')) {
-    $('.menu-open').prop('checked', false);
-  }
-})
+var body = document.getElementsByTagName('body')[0];
+
+body.onclick = function() {
+    // if($('.modal').hasClass('show') && $('.menu-open:checked')) {
+    //   console.log('hi')
+    //   // $('.modal').removeClass('show');
+    // }
+    if ($('.modal').hasClass('show')) {
+      $('.menu-open').prop('disabled', true);
+    }
+    if (!$('.modal').hasClass('show')) {
+      $('.menu-open').prop('disabled', false);
+    }
+}
