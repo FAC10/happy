a1a534eb819ea3a1aa590644d7ec7b98b7247a8d
Joey Scott <joannascott29@hotmail.co.uk>
2017-05-26 12:33:41 +0100

    Add functionality to write a message screen Relates #98

--- public/record.js
+++ public/record.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  $('#pencil').on('click', () => {
+    setTimeout(function () {
+        $('.record-text').show();
+    }, 1100);
+    $('.menu-open-button').on('click', () => {
+      $('.record-text').hide();
+      $('.record--submit').val('Save');
+      $('.record--text').prop('disabled', false);
+      $('.record--text').val('');
+      $('.record--text').css('background-color','#fff');
+    });
+  });
+
   $('#camera').on('click', () => {
     setTimeout(function () {
         $('.record-photo').show();
-    }, 1050);
+    }, 1100);
     $('.menu-open-button').on('click', () => {
       $('.record-photo').hide();
     });
@@ -12,10 +25,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   $('#microphone').on('click', () => {
     setTimeout(function () {
         $('.record-voice').show();
-    }, 1050);
+    }, 1100);
     $('.menu-open-button').on('click', () => {
       $('.record-voice').hide();
     });
+  });
+
+  $('.record--submit').on('click', () => {
+    $('.record--submit').val('Saved!');
+    $('.record--text').prop('disabled', true);
+    $('.record--text').css('background-color','#fafafa');
   });
 
 });
